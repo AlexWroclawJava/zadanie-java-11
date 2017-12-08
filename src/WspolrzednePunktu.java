72ca61fcbd6c8664fee9780feb3cfa2e1c9f0a29
AlexWroclawJava <aleksander.szlachcic@gmail.com>
2017-12-08 14:05:29 +0100

    zadanie-java-11

---
+++ src/WspolrzednePunktu.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class WspolrzednePunktu {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj X: ");
+        int wspolrzednaX = scan.nextInt();
+
+        System.out.println("Podaj Y: ");
+        int wspolrzednaY = scan.nextInt();
+
+        if (wspolrzednaX > 0 && wspolrzednaY > 0) {
+            System.out.println("Punkt(" + wspolrzednaX + "," + wspolrzednaY + ")" + "leży w I ćwiartce układu współrzędnych");
+        } else if (wspolrzednaX > 0 && wspolrzednaY < 0) {
+            System.out.println("Punkt(" + wspolrzednaX + "," + wspolrzednaY + ")" + "leży w IV ćwiartce układu współrzędnych");
+        } else if (wspolrzednaX < 0 && wspolrzednaY > 0) {
+            System.out.println("Punkt(" + wspolrzednaX + "," + wspolrzednaY + ")" + "leży w II ćwiartce układu współrzędnych");
+        } else {
+            System.out.println("Punkt(" + wspolrzednaX + "," + wspolrzednaY + ")" + "leży w III ćwiartce układu współrzędnych");
+        }
+    }
+}
